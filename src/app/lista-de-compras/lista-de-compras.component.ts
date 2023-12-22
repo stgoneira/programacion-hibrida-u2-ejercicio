4d0cb13b0358aca92d3670a6252ad03af21712c0
stgoneira <stgoneiran@gmail.com>
2023-12-22 03:23:46 -0300

    Componente ListaDeCompras carga y muestra componentes: ListaDeProductos y FormularioProducto

--- src/app/lista-de-compras/lista-de-compras.component.ts
+++ src/app/lista-de-compras/lista-de-compras.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormularioProductoComponent } from '../formulario-producto/formulario-producto.component';
+import { ListaDeProductosComponent } from '../lista-de-productos/lista-de-productos.component';
 
 @Component({
   selector: 'app-lista-de-compras',
   templateUrl: './lista-de-compras.component.html',
   styleUrls: ['./lista-de-compras.component.scss'],
+  standalone: true,
+  imports: [FormularioProductoComponent, ListaDeProductosComponent]
 })
 export class ListaDeComprasComponent  implements OnInit {
 
