7aadbf2c959b9a9da13001dc3c5ae56bff13ae6d
stgoneira <stgoneiran@gmail.com>
2023-12-22 03:24:44 -0300

    Diseño de componente FormularioProducto

--- src/app/formulario-producto/formulario-producto.component.ts
+++ src/app/formulario-producto/formulario-producto.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { IonList, IonItem, IonInput, IonButton, IonIcon } from '@ionic/angular/standalone'
+import { addIcons } from 'ionicons'
+import { addCircleOutline } from 'ionicons/icons'
 
 @Component({
   selector: 'app-formulario-producto',
   templateUrl: './formulario-producto.component.html',
   styleUrls: ['./formulario-producto.component.scss'],
+  standalone: true,
+  imports: [IonList, IonItem, IonInput, IonButton, IonIcon]
 })
 export class FormularioProductoComponent  implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({
+      addCircleOutline
+    })
+  }
 
   ngOnInit() {}
 
