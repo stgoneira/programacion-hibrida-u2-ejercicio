a66d5fa5e563368039d5164afeb1ce5cfc3bc4e1
stgoneira <stgoneiran@gmail.com>
2023-12-22 03:24:26 -0300

    Diseño de pantalla de componente ListaDeProductos

--- src/app/lista-de-productos/lista-de-productos.component.ts
+++ src/app/lista-de-productos/lista-de-productos.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import {IonList, IonItem, IonLabel, IonCheckbox} 
+                          from '@ionic/angular/standalone'
+import { Producto } from '../modelo/producto'
 
 @Component({
   selector: 'app-lista-de-productos',
   templateUrl: './lista-de-productos.component.html',
   styleUrls: ['./lista-de-productos.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonList, IonItem, IonLabel, IonCheckbox]
 })
 export class ListaDeProductosComponent  implements OnInit {
+
+  productos:Producto[] = [
+    new Producto("Arroz", false)
+    ,new Producto("Salsa de Tomates", true)
+    ,new Producto("Espirales", false)
+    ,new Producto("Agua mineral", true)
+  ]
 
   constructor() { }
 
