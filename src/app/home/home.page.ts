7e4b966cc930a3f04837c616ca33d25ac215bc16
stgoneira <stgoneiran@gmail.com>
2023-12-22 03:22:49 -0300

    Home page muestra componente ListaDeCompras

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ListaDeComprasComponent } from '../lista-de-compras/lista-de-compras.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [ListaDeComprasComponent, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor() {}
