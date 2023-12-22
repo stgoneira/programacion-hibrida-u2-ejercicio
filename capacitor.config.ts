7ebfdf72bdfb69dd7032d4680281aae1d54dfffa
stgoneira <stgoneiran@gmail.com>
2023-12-22 00:48:44 -0300

    proyecto recién iniciado

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph.u2.ej3',
+  appName: 'ph-u2-ej3',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
