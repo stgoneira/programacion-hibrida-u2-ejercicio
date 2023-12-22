efb9683f631c53949356b19b2cb88786cc2b1ada
stgoneira <stgoneiran@gmail.com>
2023-12-22 11:03:29 -0300

    RouterModule - Navegación entre Home y Configuracion page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 import { IonButtons, IonButton, IonIcon, 
          IonHeader, IonToolbar, IonTitle, IonContent } 
-                    from '@ionic/angular/standalone';
-import { ListaDeComprasComponent } from '../lista-de-compras/lista-de-compras.component';
-import { addIcons } from 'ionicons';
+                    from '@ionic/angular/standalone'
+import { ListaDeComprasComponent } from '../lista-de-compras/lista-de-compras.component'
+import { addIcons } from 'ionicons'
 import { settingsOutline } from 'ionicons/icons'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [ListaDeComprasComponent, IonHeader, IonToolbar, IonTitle, IonContent, 
+  imports: [ListaDeComprasComponent, RouterModule, IonHeader, IonToolbar, IonTitle, IonContent, 
             IonButtons, IonButton, IonIcon],
 })
 export class HomePage {
